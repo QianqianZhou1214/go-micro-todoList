67173dc43fe40caa013f04bfd4f8816b0ba30729
Qianqian Zhou <qqzh1214@gmail.com>
2025-11-03 21:01:24 +0100

    Setup DB and config

---
+++ user/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/ini.v1"
+)
+
+var (
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	DbName     string
+)
+
+func init() {
+	file, err := ini.Load("./config/config.ini")
+	if err != nil {
+		fmt.Println("Not able to load config.ini: ", err)
+	}
+	LoadMysqlData(file)
+	path := strings.Join([]string{DbUser, ":", DbPassWord, "@tcp(", DbHost, ":", DbPort, ")/", DbName, "?charset=utf8&parseTime=True"}, "")
+	model.Database(path)
+}
+
+func LoadMysqlData(file *ini.File) {
+	Db = file.Section("mysql").Key("Db").String()
+	DbHost = file.Section("mysql").Key("DbHost").String()
+	DbPort = file.Section("mysql").Key("DbPort").String()
+	DbUser = file.Section("mysql").Key("DbUser").String()
+	DbPassWord = file.Section("mysql").Key("DbPassWord").String()
+	DbName = file.Section("mysql").Key("DbName").String()
+}
---
+++ user/model/user.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	gorm.Model
+	Username       string `gorm:"unique"`
+	PasswordDigest string
+}
+
+const (
+	PassWordCost = 12 // password hash level
+)
+
+// set hashed Password
+func (user *User) SetPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), PassWordCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordDigest = string(bytes)
+	return nil
+}
+
+// Password check
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordDigest), []byte(password))
+	return err == nil
+}
