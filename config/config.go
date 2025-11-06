fb73189dfffb4fa77333118c006949e2c2e5a257
Qianqian Zhou <qqzh1214@gmail.com>
2025-11-06 22:13:52 +0100

    set config DB

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+
+	"gopkg.in/ini.v1"
+)
+
+var (
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassword string
+	DbName     string
+	Charset    string
+)
+
+func init() {
+	file, err := ini.Load("./config/config.ini")
+	if err != nil {
+		fmt.Println("config.ini not found", err)
+	}
+	LoadMySqlData(file)
+}
+
+func LoadMySqlData(file *ini.File) {
+	DbHost = file.Section("mysql").Key("DbHost").String()
+	DbPort = file.Section("mysql").Key("DbPort").String()
+	DbUser = file.Section("mysql").Key("DbUser").String()
+	DbPassword = file.Section("mysql").Key("DbPassword").String()
+	DbName = file.Section("mysql").Key("DbName").String()
+	Charset = file.Section("mysql").Key("Charset").String()
+}
